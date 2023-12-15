c5e76dc466dc7565704123caa7068e0a31a70bd2
aurinilson.fb@gmail.com <152084625+aurinilson@users.noreply.github.com>
2023-12-14 22:31:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function calcularNivel(vitorias, derrotas) {
+    // Calcula o saldo de Ranqueadas
+    const saldoVitorias = vitorias - derrotas;
+
+    // Determina o nível com base no saldo de vitórias
+    let nivel;
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    // Retorna a mensagem formatada
+    return `O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`;
+}
+
+// Exemplo de uso da função
+const resultado = calcularNivel(30, 15);
+console.log(resultado);
